7abf4cb5714396fd6a1a294ffcea152a5897a7f2
Nico Reincheld <nicoreincheld@gmail.com>
2024-09-19 16:03:17 -0400

    Add OrderSummary styling and selected item event handler

--- src/components/POS/OrderSummary/OrderSummary.jsx
+++ src/components/POS/OrderSummary/OrderSummary.jsx
@@ -14,7 +14,15 @@ function OrderSummary({
         </div>
         <div className="order__list">
           {order.map((item) => (
-            <div className="order__item" key={item.id}>
+            <div
+              className={`order__item ${
+                item.id === selectedItemId ? 'order__item--selected' : ''
+              } `}
+              key={item.id}
+              onClick={() => {
+                handleSelectItem(item.id);
+              }}
+            >
               <p className="order_item-name">{item.name}...</p>
               <p className="order_item-price">...{item.price}</p>
             </div>
@@ -22,13 +30,16 @@ function OrderSummary({
         </div>
         <div className="order__footer">
           <div className="order__footer-container">
-            <h1 className="order__footer-subtotal">subtotal</h1>
+            <h1 className="order__footer-subtotal">subtotal...</h1>
+            <h1 className="order__footer-totals">...sub</h1>
           </div>
           <div className="order__footer-container">
-            <h1 className="order__footer-tax">tax</h1>
+            <h1 className="order__footer-tax">tax...</h1>
+            <h1 className="order__footer-totals">...tax</h1>
           </div>
           <div className="order__footer-container">
-            <h1 className="order__footer-total">total</h1>
+            <h1 className="order__footer-total">total...</h1>
+            <h1 className="order__footer-totals">...total</h1>
           </div>
         </div>
         <div className="order__number">Trans: 1234</div>
